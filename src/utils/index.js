2664c74c673b9040fb93fa01cedc5d7c97fed4c7
Amram1997 <amramananyan9@gmail.com>
2022-03-27 22:58:44 +0400

    feat: utils files urls

---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { ALL_PRODUCTS, PRODUCT_BY_ID } from "./urls";
---
+++ src/utils/urls.js
@@ -0,0 +1,3 @@
+const ALL_PRODUCTS = "https://fakestoreapi.com/products";
+const PRODUCT_BY_ID = "https://fakestoreapi.com/products/:id";
+export { ALL_PRODUCTS, PRODUCT_BY_ID };
